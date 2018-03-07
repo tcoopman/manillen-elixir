76d64cfeeb2ab996c264977b3475084f6bc1e447
Thomas Coopman <thomas.coopman@gmail.com>
2018-03-07 21:44:43 +0100

    start genserver implemantion

--- lib/manillen.ex
+++ lib/manillen.ex
@@ -1,13 +1,26 @@
 defmodule Manillen do
+  use GenServer
 
-  def validate_trick([_, _, _, _] = cards) do
-    unique_cards = Enum.uniq(cards)
-
-    case unique_cards == cards do
-      false -> {:error, :duplicate_card}
-      true -> :ok
-    end
+  def start_link() do
+    GenServer.start_link(__MODULE__, nil, name: :manillen)
   end
 
-  def validate_trick(_), do: {:error, :incorrect_number_of_moves}
+  def play(player, card) do
+    GenServer.call(:manillen, {:play, player, card})
+  end
+
+  def handle_call({:play, _player, _card}, _from, state) do
+    {:reply, :ok, state}
+  end
+
+  # def validate_trick([_, _, _, _] = cards) do
+  #   unique_cards = Enum.uniq(cards)
+
+  #   case unique_cards == cards do
+  #     false -> {:error, :duplicate_card}
+  #     true -> :ok
+  #   end
+  # end
+
+  # def validate_trick(_), do: {:error, :incorrect_number_of_moves}
 end
--- test/manillen_test.exs
+++ test/manillen_test.exs
@@ -2,30 +2,19 @@ defmodule ManillenTest do
   use ExUnit.Case
   doctest Manillen
 
+  test "Start a new game" do
+    assert {:ok, _pid} = Manillen.start_link()
+  end
+
   describe "Validate a trick" do
-    test "valid trick" do
-      assert :ok ==
-               Manillen.validate_trick([
-                 {:hearts, :manille},
-                 {:hearts, :seven},
-                 {:hearts, :eight},
-                 {:hearts, :nine}
-               ])
+    setup do
+      Manillen.start_link()
+
+      :ok
     end
 
-    test "invalid trick" do
-      assert {:error, :duplicate_card} ==
-               Manillen.validate_trick([
-                 {:hearts, :manille},
-                 {:hearts, :manille},
-                 {:hearts, :eight},
-                 {:hearts, :nine}
-               ])
-    end
-
-    test "invalid trick, not enough moves" do
-      assert {:error, :incorrect_number_of_moves} =
-        Manillen.validate_trick([])
+    test "A first valid move" do
+      :ok = Manillen.play(:player1, {:hearts, :eight})
     end
   end
 end
